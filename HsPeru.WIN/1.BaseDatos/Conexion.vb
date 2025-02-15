0501afe945d91b674de288c6bd08d1aeb2b35fab
jaipe <millepacheco87@gmail.com>
2025-02-15 13:39:41 -0500

    2025-02-15 cambio de conexion

--- HsPeru.WIN/1.BaseDatos/Conexion.vb
+++ HsPeru.WIN/1.BaseDatos/Conexion.vb
@@ -4,7 +4,7 @@ Module Conexion
     Public Cn As OdbcConnection
 
     Public Sub SetConexion()
-        Dim connectionString As String = "Driver={MySQL ODBC 3.51 Driver};SERVER=localhost;port=3306;Database=dbhsgestion;Uid=root;Pwd=samanthafox;"
+        Dim connectionString As String = "Driver={MySQL ODBC 3.51 Driver};SERVER=www.amkdelivery.com;port=3306;Database=dbhsgestion;Uid=root;Pwd=jarley2703..;"
 
         Cn = New OdbcConnection(connectionString)
         If Cn.State = System.Data.ConnectionState.Open Then
