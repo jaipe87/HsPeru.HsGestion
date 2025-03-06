21b0354ee7bbd52f36d54cd69768f71af1194e75
jaipe87 <millepacheco87@gmail.com>
2025-03-06 18:02:35 -0500

    2025-03-06 18:02 Intentando generar pdf y excel, no deja instalar dependencias

---
+++ HsPeru.WIN/4.Helpers/GeneraReporte.vb
@@ -0,0 +1,60 @@
+﻿
+Imports System.IO
+Imports ClosedXML.Excel
+Imports iTextSharp.text
+Imports iTextSharp.text.pdf
+'NO DEJA DESCARGAR LAS DEPENDENCIAS!!!!
+Public Class GeneraReporte
+    Public Function ExportarExcel(data As DataTable) As Byte()
+        Using wb As New XLWorkbook()
+            Dim ws = wb.Worksheets.Add("Reporte")
+            ws.Cell(1, 1).Value = "Empresa: Mi Empresa"
+            ws.Cell(2, 1).Value = "RUC: 123456789"
+            ws.Cell(3, 1).Value = "Fecha: " & DateTime.Now.ToString("dd/MM/yyyy")
+            ws.Cell(5, 1).InsertTable(data)
+            Using ms As New MemoryStream()
+                wb.SaveAs(ms)
+                Return ms.ToArray()
+            End Using
+        End Using
+    End Function
+
+    ' Exportar datos a PDF
+    Public Function ExportarPDF(data As DataTable) As Byte()
+        Dim doc As New Document(PageSize.A4)
+        Using ms As New MemoryStream()
+            Dim writer = PdfWriter.GetInstance(doc, ms)
+            doc.Open()
+
+            ' Agregar imagen (logo)
+            Dim logo As Image = Image.GetInstance("C:\ruta\logo.png")
+            logo.ScaleAbsolute(100, 50)
+            doc.Add(logo)
+
+            ' Título
+            Dim titulo As New Paragraph("Reporte de Datos") With {.Alignment = Element.ALIGN_CENTER}
+            doc.Add(titulo)
+
+            ' Información de la empresa
+            doc.Add(New Paragraph("Empresa: Mi Empresa"))
+            doc.Add(New Paragraph("RUC: 123456789"))
+            doc.Add(New Paragraph("Fecha: " & DateTime.Now.ToString("dd/MM/yyyy")))
+            doc.Add(New Paragraph(" "))
+
+            ' Crear tabla PDF
+            Dim table As New PdfPTable(data.Columns.Count)
+            For Each col As DataColumn In data.Columnse
+                table.AddCell(New PdfPCell(New Phrase(col.ColumnName)))
+            Next
+            For Each row As DataRow In data.Rows
+                For Each item In row.ItemArray
+                    table.AddCell(New PdfPCell(New Phrase(item.ToString())))
+                Next
+            Next
+            doc.Add(table)
+
+            doc.Close()
+            Return ms.ToArray()
+        End Using
+    End Function
+End Class
